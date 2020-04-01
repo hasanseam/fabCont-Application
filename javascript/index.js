149f76a0b3bd5f1c3fe46c5c2bf9c5127037472b
Christian Maeder <c.maeder@uni-bremen.de>
2020-04-01 14:24:15 +0200

    two wallets, two admin, user1 and user2

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const FabCont = require('./lib/fabcont');
+
+module.exports.FabCont = FabCont;
+module.exports.contracts = [ FabCont ];
